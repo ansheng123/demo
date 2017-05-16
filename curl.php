870e48ae8eee30fd38c2c4bd6788e043f1bf3864
gaoxu <476961900@qq.com>
2017-05-16 18:32:27 +0800

    用curl实现网络爬虫

---
+++ curl.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-type:text/html; charset=UTF-8');
+error_reporting(E_ALL &~E_WARNING &~E_NOTICE);
+
+
+$obj = curl_init();
+
+$options = array(
+            CURLOPT_URL => "http://www.baidu.com",
+                    CURLOPT_RETURNTRANSFER => true);
+
+curl_setopt_array($obj, $options);
+
+$output = curl_exec($obj);
+
+curl_close($url);
+
+echo str_replace("百度", "屌丝", $output);
+
+date_default_timezone_set('PRC');
